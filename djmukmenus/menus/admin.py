63f88c1c8acf2f2fd91b7a0c7d141147d4916afe
Jon Parrish <jonparrish@rocketmail.com>
2012-07-13 15:50:11 -0500

    updated models

--- djmukmenus/menus/admin.py
+++ djmukmenus/menus/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from menus.models import Menu
+from menus.forms import *
 
 class MenuAdmin(admin.ModelAdmin):
-	list_display = ('title', 'description')
-	list_display_links = ('title',)
+	form = DiningOptionsForm
+
+	list_display = ('restaurant', 'genre')
+	list_display_links = ('restaurant',)
+	list_filter = ('restaurant', 'genre')
 	list_per_page = 50
-	search_fields = ['title', 'description',]
+	search_fields = ['restaurant', 'genre',]
 
 admin.site.register(Menu, MenuAdmin)
